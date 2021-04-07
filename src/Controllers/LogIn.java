015896155f35dd3bd5bbee1fba709d2c661235ad
Antonb28 <62889116+Antonb28@users.noreply.github.com>
2021-04-07 17:31:53 +0800

    LogIn and Register Animation Added

--- src/Controllers/LogIn.java
+++ src/Controllers/LogIn.java
@@ -1,5 +1,39 @@
 package Controllers;
 
-public class LogIn {
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+import javafx.event.ActionEvent;
 
+public class LogIn {
+    @FXML
+    AnchorPane LogIn, Register;
+
+    void LogInScreen(){
+        //this code basically moves the anchorpane lang, no need to set false
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(1),LogIn);
+        translateTransition.setByX(-600);
+        translateTransition.play();
+        //just to pause
+        TranslateTransition translateTransition2 = new TranslateTransition(Duration.seconds(1),Register);
+        translateTransition2.setByX(427);
+        translateTransition2.play();
+    }
+
+    public void Register(ActionEvent actionEvent){
+        //for this part put the pop up box?? to confirm if all info is correct
+        LogInScreen();
+
+    }
+
+    public void RegisterScreen(ActionEvent actionEvent) {
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(1),LogIn);
+        translateTransition.setByX(600);
+        translateTransition.play();
+        TranslateTransition translateTransition2 = new TranslateTransition(Duration.seconds(1),Register);
+        translateTransition2.setByX(-427);
+        translateTransition2.play();
+    }
 }
+
