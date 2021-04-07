551428e4fa0a3518a6a15ed03b1d44c9cbc017b9
Antonb28 <62889116+Antonb28@users.noreply.github.com>
2021-04-07 16:05:48 +0800

    App Set Up

---
+++ src/Controllers/Main.java
@@ -0,0 +1,40 @@
+package Controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    Stage loginstage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.loginstage=primaryStage;
+        LoginWindow();
+    }
+
+    private void LoginWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../FXML Files/LogIn.fxml"));
+            AnchorPane pane = loader.load();
+
+            Scene scene = new Scene(pane);
+            loginstage.setScene(scene);
+            loginstage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
